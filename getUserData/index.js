66e36f327792a22580bf46ba511dc883e8c51375
John Martin <jrmartin182@gmail.com>
2020-07-28 11:31:58 -0600

    added documentClient and moved get into promise

--- getUserData/index.js
+++ getUserData/index.js
@@ -6,21 +6,24 @@ AWS.config.update({region: "us-west-1"});
 //handler is the fx being exported with index being the exported file
 //takes 3 args by default: event- info that triggered the lambda; context- methods and props that provide
 // information abvout the invocation, function and execution environment. 
-exports.handler = function(event, context, callback){
+exports.handler = async (event, context) => {
     const ddb = new AWS.DynamoDB({apiVersion: "2012-10-08"});
+    //gets rid of needing to go into Dynamo's S value to get id
+    const documentClient = new AWS.DynamoDB.DocumentClient({region: "us-west-1"});
+
     const params = {
         TableName: "Users",
         Key: {
-            id: {
-                S: "1234"
-            }
+            id: "12345"
         }
-    }
+    };
 
-    ddb.getItem(params, (err, data) => {
-        if(err){
-            console.log(err);
-        }
+    try {
+        const data = await documentClient.get(params).promise();
         console.log(data);
-    })
+    } catch(err) {
+        console.log(err);
+    };
+
+
 }
